2cee58db67e490506b05f2705b190a626329aac4
David Richey <darichey1@gmail.com>
2022-05-27 02:46:41 -0500

    Add keyboard shortcut for flameshot

--- home/xmonad-config/xmonad.hs
+++ home/xmonad-config/xmonad.hs
@@ -5,4 +5,6 @@ main :: IO ()
 main = xmonad $ def
     { terminal = "alacritty" }
     `additionalKeysP`
-    [ ("M-r", spawn "rofi -show drun") ]
+    [ ("M-r", spawn "rofi -show drun") -- Alt+r => Application runner
+    , ("C-S-4", spawn "flameshot gui") -- Ctrl+Shift+4 => Area selection screenshot
+    ]
