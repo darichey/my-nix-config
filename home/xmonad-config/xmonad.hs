78bcb6b2fc9bcb0d10134aa452513365fdaa8c5a
David Richey <darichey1@gmail.com>
2022-05-30 00:55:09 -0500

    Use smartBorders for xmonad layout

--- home/xmonad-config/xmonad.hs
+++ home/xmonad-config/xmonad.hs
@@ -1,9 +1,26 @@
 import XMonad
 import XMonad.Util.EZConfig
+import XMonad.Layout.NoBorders (smartBorders)
+
+myLayout = smartBorders $ tiled ||| Mirror tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
 
 main :: IO ()
 main = xmonad $ def
-    { terminal = "alacritty" }
+    { terminal = "alacritty"
+    , layoutHook = myLayout
+    }
     `additionalKeysP`
     [ ("M-r", spawn "rofi -show drun") -- Alt+r => Application runner
     , ("C-S-4", spawn "flameshot gui") -- Ctrl+Shift+4 => Area selection screenshot
