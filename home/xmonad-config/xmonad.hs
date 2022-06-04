c0f89a7d7283f4b188900a63b986276b62b62338
David Richey <darichey1@gmail.com>
2022-06-04 01:55:20 -0500

    Add terminal scratchpad

--- home/xmonad-config/xmonad.hs
+++ home/xmonad-config/xmonad.hs
@@ -5,6 +5,11 @@ import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 import XMonad.Hooks.EwmhDesktops (ewmhFullscreen)
+import XMonad.Util.Scratchpad (scratchpadSpawnActionTerminal, scratchpadManageHookDefault)
+import XMonad.Util.NamedScratchpad
+import qualified XMonad.StackSet as W
+
+myTerminal = "alacritty"
 
 myLayout = avoidStruts $ smartBorders $ tiled ||| Mirror tiled ||| Full
   where
@@ -20,21 +25,26 @@ myLayout = avoidStruts $ smartBorders $ tiled ||| Mirror tiled ||| Full
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
 
-myManageHook :: ManageHook
 myManageHook =
     composeAll
       [ isFullscreen               --> doFullFloat
       , className =? "Pavucontrol" --> doFloat
       , className =? "pinentry"    --> doFloat
+      , namedScratchpadManageHook myScratchPads
       ]
+
+myScratchPads =
+  [ NS "terminal" (myTerminal ++ " -t scratchpad") (title =? "scratchpad") defaultFloating
+  ]
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ desktopConfig
-    { terminal = "alacritty"
+    { terminal = myTerminal
     , layoutHook = myLayout
-    , manageHook = myManageHook <+> manageHook desktopConfig
+    , manageHook = myManageHook <+> manageHook desktopConfig <+> scratchpadManageHookDefault
     }
     `additionalKeysP`
     [ ("M-r", spawn "rofi -show drun") -- Alt+r => Application runner
     , ("C-S-4", spawn "flameshot gui") -- Ctrl+Shift+4 => Area selection screenshot
+    , ("<F12>", namedScratchpadAction myScratchPads "terminal") -- F12 => Toggle terminal scratchpad
     ]
