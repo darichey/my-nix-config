28357c5a2715297e173eb5da0b47f660accff403
David Richey <darichey1@gmail.com>
2022-09-02 15:16:29 -0500

    Disable intellij keybindings vscode extension

--- home/home.nix
+++ home/home.nix
@@ -148,12 +148,12 @@
               sha256 = "115y86w6n2bi33g1xh6ipz92jz5797d3d00mr4k8dv5fz76d35dd";
             }
             # Not in nixpkgs
-            {
-              name = "intellij-idea-keybindings";
-              publisher = "k--kato";
-              version = "1.5.1";
-              sha256 = "sha256-X+q43p455J9SHBEvin1Umr4UfQVCI8vnIkoH5/vUUJs=";
-            }
+            # {
+            #   name = "intellij-idea-keybindings";
+            #   publisher = "k--kato";
+            #   version = "1.5.1";
+            #   sha256 = "sha256-X+q43p455J9SHBEvin1Umr4UfQVCI8vnIkoH5/vUUJs=";
+            # }
             {
               name = "direnv";
               publisher = "mkhl";
