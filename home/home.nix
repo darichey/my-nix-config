4e9f30f0d01854cbd9089fddb69dba626ec25a3b
David Richey <darichey1@gmail.com>
2023-08-01 21:48:14 -0500

    programs.zsh.enableSyntaxHighlighting -> programs.zsh.syntaxHighlighting.enable

--- home/home.nix
+++ home/home.nix
@@ -95,7 +95,7 @@
       enable = true;
       enableAutosuggestions = true;
       enableCompletion = true;
-      enableSyntaxHighlighting = true;
+      syntaxHighlighting.enable = true;
 
       oh-my-zsh = {
         enable = true;
